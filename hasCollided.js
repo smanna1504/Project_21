70967d1e49badf39342be611be6b0397160803c4
SAYAN MANNA <73745559+smanna1504@users.noreply.github.com>
2020-11-12 17:13:42 +0530

    Add files via upload

---
+++ hasCollided.js
@@ -0,0 +1,9 @@
+function hasCollided(lbullet,lwall){
+    bulletRightEdge=lbullet.x+lbullet.width;
+   wallLeftEdge=lwall.x;
+    if(bulletRightEdge>=wallLeftEdge){
+       return true;
+    }
+   else{ return false;
+   }
+  }
